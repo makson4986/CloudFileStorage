bd8f5d53b15c1c425e408ecb3632bf0a4f2b0ec5
makson <maksbatkaev06@gmail.com>
2025-06-21 18:51:19 +0300

    fix: timeout JSESSIONID

--- src/main/java/com/makson/cloudfilestorage/config/SessionConfig.java
+++ src/main/java/com/makson/cloudfilestorage/config/SessionConfig.java
@@ -1,16 +1,20 @@
 package com.makson.cloudfilestorage.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
+import java.time.Duration;
+
 @Configuration
 public class SessionConfig {
     @Bean
-    public CookieSerializer cookieSerializer() {
+    public CookieSerializer cookieSerializer(@Value("${spring.session.timeout}") Duration sessionTimeOut) {
         DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
         cookieSerializer.setCookieName("JSESSIONID");
+        cookieSerializer.setCookieMaxAge((int) sessionTimeOut.getSeconds());
         cookieSerializer.setCookiePath("/");
         return cookieSerializer;
     }
