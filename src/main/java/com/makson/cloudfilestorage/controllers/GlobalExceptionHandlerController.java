c7416187dd9b0b46bbfa80492d451c5c26fbe372
makson <maksbatkaev06@gmail.com>
2025-07-06 12:28:40 +0300

    refactor: handle exception by statuses and not by errors

--- src/main/java/com/makson/cloudfilestorage/controllers/GlobalExceptionHandlerController.java
+++ src/main/java/com/makson/cloudfilestorage/controllers/GlobalExceptionHandlerController.java
@@ -1,6 +1,7 @@
 package com.makson.cloudfilestorage.controllers;
 
 import com.makson.cloudfilestorage.dto.ErrorDto;
+import com.makson.cloudfilestorage.exceptions.ResourceAlreadyExistException;
 import com.makson.cloudfilestorage.exceptions.ResourceNotFoundException;
 import com.makson.cloudfilestorage.exceptions.UserAlreadyExistException;
 import lombok.extern.slf4j.Slf4j;
@@ -10,15 +11,43 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MultipartException;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandlerController {
-    @ExceptionHandler(UserAlreadyExistException.class)
-    public ResponseEntity<?> handleRegistrationException(Exception ex) {
+
+    @ExceptionHandler({
+            ResourceAlreadyExistException.class,
+            UserAlreadyExistException.class
+    })
+    public ResponseEntity<?> handleConflictException(Exception ex) {
         log.warn(ex.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorDto(ex.getMessage()));
     }
+
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            MultipartException.class
+    })
+    public ResponseEntity<?> handleBadRequestException(Exception ex) {
+        ErrorDto errorDto;
+
+        if (ex instanceof MethodArgumentNotValidException) {
+            errorDto = new ErrorDto(
+                    ((MethodArgumentNotValidException) ex).getFieldErrors().stream()
+                            .map(er -> er.getField() + ": " + er.getDefaultMessage())
+                            .toList()
+                            .getFirst()
+            );
+        } else {
+            errorDto = new ErrorDto("Files are invalid or missing");
+        }
+
+        log.warn(errorDto.message());
+        return ResponseEntity.badRequest().body(errorDto);
+    }
+
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<?> handleAuthenticationException(Exception ex) {
@@ -30,18 +59,6 @@ public class GlobalExceptionHandlerController {
     public ResponseEntity<?> handleGetInfoResourceException(Exception ex) {
         log.warn(ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDto(ex.getMessage()));
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleValidationException(Exception ex) {
-        log.warn(ex.getMessage());
-        ErrorDto errorDto = new ErrorDto(
-                ((MethodArgumentNotValidException) ex).getFieldErrors().stream()
-                        .map(er -> er.getField() + ": " + er.getDefaultMessage())
-                        .toList()
-                        .getFirst()
-        );
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDto);
     }
 
     @ExceptionHandler(Exception.class)
