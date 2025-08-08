1adac9712d93d7bd04656de35239cbaeeff84209
makson <maksbatkaev06@gmail.com>
2025-08-08 10:43:42 +0300

    feat: uploading files without extension

--- src/main/java/com/makson/cloudfilestorage/validation/impl/PathValidator.java
+++ src/main/java/com/makson/cloudfilestorage/validation/impl/PathValidator.java
@@ -6,7 +6,8 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class PathValidator implements ConstraintValidator<Path, String> {
-    private final String CORRECT_PATH = "^([a-zA-Zа-яА-ЯёЁ0-9._\\- ]+/)*([a-zA-Zа-яА-ЯёЁ0-9._\\- ]+\\.[a-zA-Zа-яА-ЯёЁ0-9]+/?)?$";
+    private final String CORRECT_PATH = "^([a-zA-Zа-яА-ЯёЁ0-9._\\- ]+/)*([a-zA-Zа-яА-ЯёЁ0-9._\\- ]+(\\.[a-zA-Zа-яА-ЯёЁ0-9]+)?/?)?$";
+
 
     @Override
     public boolean isValid(String  value, ConstraintValidatorContext context) {
